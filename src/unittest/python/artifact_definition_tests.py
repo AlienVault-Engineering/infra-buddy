26fe16c814548d2567381a10004e3b6fdb87933b
rspitler <rspitler@alienvault.com>
2017-11-10 13:43:56 -0600

    Improve artifact definition tests with type validation

--- src/unittest/python/artifact_definition_tests.py
+++ src/unittest/python/artifact_definition_tests.py
@@ -6,7 +6,7 @@ from infra_buddy.aws.cloudformation import CloudFormationBuddy
 from infra_buddy.aws.s3 import S3Buddy
 from infra_buddy.commandline import cli
 from infra_buddy.commands.generate_artifact_manifest import command
-from infra_buddy.context.artifact_definition import ArtifactDefinition
+from infra_buddy.context.artifact_definition import ArtifactDefinition, ECSArtifactDefinition, S3ArtifactDefinition
 from infra_buddy.context.deploy_ctx import DeployContext
 from infra_buddy.deploy.cloudformation_deploy import CloudFormationDeploy
 from infra_buddy.template.template import NamedLocalTemplate
@@ -28,16 +28,19 @@ class ArtifactDefinitionTestCase(ParentTestCase):
         self.assertEqual(art.artifact_id,"39","Did not parse artifact id")
         self.assertEqual(art.artifact_location,"https://docker.io/my-registry/artifact","Did not parse artifact location")
         self.assertEqual(art.artifact_type,"container","Did not parse artifact type")
+        self.assertTrue(isinstance(art,ECSArtifactDefinition),"Did not return correct class")
         artifact_directory = self._get_resource_path('artifact_directory_tests/artifact_s3_definition')
         art = ArtifactDefinition.create_from_directory(artifact_directory)
         self.assertEqual(art.artifact_id,"39","Did not parse artifact id")
         self.assertEqual(art.artifact_location,"s3_bucket/path","Did not parse artifact location")
         self.assertEqual(art.artifact_type,"s3","Did not parse artifact type")
+        self.assertTrue(isinstance(art,S3ArtifactDefinition),"Did not return correct class")
         artifact_directory = self._get_resource_path('artifact_directory_tests/legacy_artifact_definition')
         art = ArtifactDefinition.create_from_directory(artifact_directory)
         self.assertEqual(art.artifact_id,"39","Did not parse artifact id")
         self.assertEqual(art.artifact_location,"https://docker.io/my-registry/artifact","Did not parse artifact location")
         self.assertEqual(art.artifact_type,"container","Did not parse artifact type")
+        self.assertTrue(isinstance(art,ECSArtifactDefinition),"Did not return correct class")
 
     def test_save_manifest(self):
         mkdtemp = tempfile.mkdtemp()
