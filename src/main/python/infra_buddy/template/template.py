50ce182654c42f847363620bd8e4094bf95c598f
rspitler <rspitler@alienvault.com>
2019-03-28 08:24:46 -0700

    add correct import

--- src/main/python/infra_buddy/template/template.py
+++ src/main/python/infra_buddy/template/template.py
@@ -84,7 +84,8 @@ class URLTemplate(Template):
         self._prep_download()
         r = requests.get(self.download_url, stream=True)
         if r.status_code != 200:
-            print_utility.error("Template cloud not be downloaded - {body}".format(body=r.text))
+            print_utility.error("Template cloud not be downloaded - {status} {body}".format(status=r.status_code,
+                                                                                            body=r.text))
         temporary_file = tempfile.NamedTemporaryFile()
         for chunk in r.iter_content(chunk_size=1024):
             if chunk:  # filter out keep-alive new chunks
