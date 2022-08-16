b4853c16d74394c4cd4ba1461c3af90d07a79fcc
rspitler <rspitler@nudgesecurity.com>
2022-08-16 11:00:16 -0600

    update build number

--- setup.py
+++ setup.py
@@ -57,9 +57,9 @@ except subprocess.CalledProcessError as e:
 
 try:
     args = ["pyb", "clean", "install_build_dependencies", "package", "-o"]
-    if os.environ.get('TRAVIS_BUILD_NUMBER') is not None:
+    if os.environ.get('GITHUB_RUN_NUMBER') is not None:
         args.append('-P')
-        args.append('build_number=0.1.{}'.format(os.environ.get('TRAVIS_BUILD_NUMBER')))
+        args.append('build_number=0.1.{}'.format(os.environ.get('GITHUB_RUN_NUMBER')))
     subprocess.check_call(args)
     dist_dir = glob.glob(os.path.join(script_dir, "target", "dist", "*"))[0]
     for src_file in glob.glob(os.path.join(dist_dir, "*")):
